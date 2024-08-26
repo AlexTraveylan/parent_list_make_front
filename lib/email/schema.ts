840481a8be2578c82fdc032defbf599317b0ec19
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-26 20:00:42 +0200

    email service

--- lib/email/schema.ts
+++ lib/email/schema.ts
@@ -7,3 +7,15 @@ export const emailRecoverySchema = z.object({
 })
 
 export type EmailRecovery = z.infer<typeof emailRecoverySchema>
+
+export const emailConfirmationTokenSchema = z.object({
+  id: z.number(),
+  token: z.string(),
+  is_confirmed: z.boolean(),
+  created_at: z.string(),
+  user_id: z.number(),
+})
+
+export type EmailConfirmationToken = z.infer<
+  typeof emailConfirmationTokenSchema
+>
--- lib/email/service.ts
+++ lib/email/service.ts
@@ -1,7 +1,61 @@
+import { addEmailRoute, contactUserRoute } from "../api-routes"
+import { extractAuthTokenFromLocalStorage } from "../authentification/token"
+import { EmailConfirmationToken, emailConfirmationTokenSchema } from "./schema"
+
 class EmailService {
   async sendResetPasswordRequest(email: string): Promise<void> {
     // TODO : write it later
     console.log("sendResetPasswordRequest", email)
+  }
+
+  async addEmailToUser(email: string): Promise<EmailConfirmationToken> {
+    const authToken = extractAuthTokenFromLocalStorage()
+    const headers = new Headers()
+    headers.append("Authorization", authToken)
+
+    try {
+      const response = await fetch(addEmailRoute, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({
+          email: email,
+        }),
+      })
+
+      if (!response.ok) {
+        throw new Error(`Echec de la connexion ${response.status}`)
+      }
+
+      const responseJson = await response.json()
+      const emailConfirmationToken =
+        emailConfirmationTokenSchema.parse(responseJson)
+
+      return emailConfirmationToken
+    } catch (error) {
+      throw error
+    }
+  }
+
+  async contactUser(messsage: string, userId: number): Promise<void> {
+    const authToken = extractAuthTokenFromLocalStorage()
+    const headers = new Headers()
+    headers.append("Authorization", authToken)
+
+    try {
+      const response = await fetch(`${contactUserRoute}${userId}`, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify({
+          message: messsage,
+        }),
+      })
+
+      if (!response.ok) {
+        throw new Error(`Echec de la connexion ${response.status}`)
+      }
+    } catch (error) {
+      throw error
+    }
   }
 }
 export const emailService = new EmailService()
