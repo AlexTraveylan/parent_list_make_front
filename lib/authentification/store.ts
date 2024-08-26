a5438fec220059c2d2018bb54265d021212cc55e
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-26 17:27:29 +0200

    store userME

---
+++ lib/authentification/store.ts
@@ -0,0 +1,22 @@
+import { create } from "zustand"
+import { UserMe } from "./schema"
+import { authService } from "./service"
+
+type userMeStoreType = {
+  userMe: UserMe | null
+  fetchUserMe: () => Promise<void>
+  cleanUserMe: () => void
+}
+
+export const useUserMeStore = create<userMeStoreType>((set) => ({
+  userMe: null,
+  fetchUserMe: async () => {
+    try {
+      const userMe = await authService.getUserMe()
+      set({ userMe })
+    } catch (error) {
+      set({ userMe: null })
+    }
+  },
+  cleanUserMe: () => set({ userMe: null }),
+}))
