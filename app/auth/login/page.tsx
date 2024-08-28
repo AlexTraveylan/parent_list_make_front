b1bbe49d9b99a34e37ba476f46db755840603767
AlexTraveylan <monsieurdemares@gmail.com>
2024-08-28 20:25:33 +0200

    nav login/register

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -17,6 +17,7 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { InputEye } from "@/components/ui/input-password-eye"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { AuthSchemaIn, authSchemaIn } from "@/lib/authentification/schemas"
 import { authService } from "@/lib/authentification/service"
 import { useUserMeStore } from "@/lib/authentification/store"
@@ -51,56 +52,69 @@ export default function LoginPage() {
   }
 
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitleH1 className="text-2xl">
-          {"Formulaire de connexion"}
-        </CardTitleH1>
-        <CardDescription>
-          {"Connectez-vous pour accéder à votre compte"}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{"Nom d'utilisateur"}</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <div className="flex items-center">
-                    <FormLabel>{"Mot de passe"}</FormLabel>
-                    <Link
-                      className="ml-auto inline-block text-sm underline"
-                      href={authNavItems["forgotPassword"].href}
-                    >
-                      {"Mot de passe oublié"}
-                    </Link>
-                  </div>
-                  <FormControl>
-                    <InputEye {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="w-full">
-              {"Se connecter"}
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+    <>
+      <Tabs className="mx-auto max-w-sm mb-2" defaultValue="login">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="login">Connexion</TabsTrigger>
+          <TabsTrigger
+            onClick={() => router.push(authNavItems["register"].href)}
+            value="register"
+          >
+            Créer un compte
+          </TabsTrigger>
+        </TabsList>
+      </Tabs>
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitleH1 className="text-2xl">
+            {"Formulaire de connexion"}
+          </CardTitleH1>
+          <CardDescription>
+            {"Connectez-vous pour accéder à votre compte"}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{"Nom d'utilisateur"}</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex items-center">
+                      <FormLabel>{"Mot de passe"}</FormLabel>
+                      <Link
+                        className="ml-auto inline-block text-sm underline"
+                        href={authNavItems["forgotPassword"].href}
+                      >
+                        {"Mot de passe oublié"}
+                      </Link>
+                    </div>
+                    <FormControl>
+                      <InputEye {...field} />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                {"Se connecter"}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </>
   )
 }
--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import { Button } from "@/components/ui/button"
 import {
   Card,
@@ -18,10 +17,11 @@ import {
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { InputEye } from "@/components/ui/input-password-eye"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { AuthSchemaIn, authSchemaIn } from "@/lib/authentification/schemas"
 import { authService } from "@/lib/authentification/service"
 import { useUserMeStore } from "@/lib/authentification/store"
-import { navItems } from "@/lib/navigation"
+import { authNavItems, navItems } from "@/lib/navigation"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useRouter } from "next/navigation"
 import { useForm } from "react-hook-form"
@@ -52,52 +52,65 @@ export default function RegisterPage() {
   }
 
   return (
-    <Card className="mx-auto max-w-sm">
-      <CardHeader>
-        <CardTitleH1 className="text-xl">
-          {"Formulaire de création de compte"}
-        </CardTitleH1>
-        <CardDescription>
-          {
-            "Créez votre compte pour rejoindre (ou créer) la liste de parents élus de votre école"
-          }
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{"Nom d'utilisateur"}</FormLabel>
-                  <FormControl>
-                    <Input {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{"Mot de passe"}</FormLabel>
-                  <FormControl>
-                    <InputEye {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit" className="w-full">
-              {"Créer un compte"}
-            </Button>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+    <>
+      <Tabs className="mx-auto max-w-sm mb-2" defaultValue="register">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger
+            onClick={() => router.push(authNavItems["login"].href)}
+            value="login"
+          >
+            Connexion
+          </TabsTrigger>
+          <TabsTrigger value="register">Créer un compte</TabsTrigger>
+        </TabsList>
+      </Tabs>
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitleH1 className="text-xl">
+            {"Formulaire de création de compte"}
+          </CardTitleH1>
+          <CardDescription>
+            {
+              "Créez votre compte pour rejoindre (ou créer) la liste de parents élus de votre école"
+            }
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{"Nom d'utilisateur"}</FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{"Mot de passe"}</FormLabel>
+                    <FormControl>
+                      <InputEye {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                {"Créer un compte"}
+              </Button>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </>
   )
 }
